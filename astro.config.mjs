b8f55beb9d51785d3cc98fa6aba4a2b4e0a2f193
Jamie Nordmeyer <jamie@staticsphere.com>
2026-04-04 10:07:48 -0700

    Deploy attempt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://jamienordmeyer.net",
+  site: "https://staticsphere.github.io",
   legacy: {
     collectionsBackwardsCompat: true
   },
